68484db7299bcfecb31da170c4ea131ab8689fe0
madhu <madhuvanthi88@gmail.com>
2020-11-09 20:27:00 +0800

    Updates

---
+++ precache-manifest.07d85006ad59eededa2757b71ea2284e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7043aaf5e09faceca6bc",
+    "url": "/React_Calculator/static/css/main.0c13b697.chunk.css"
+  },
+  {
+    "revision": "7043aaf5e09faceca6bc",
+    "url": "/React_Calculator/static/js/main.281dde08.chunk.js"
+  },
+  {
+    "revision": "7d2a71744eb95ea6e308",
+    "url": "/React_Calculator/static/js/runtime~main.94834a62.js"
+  },
+  {
+    "revision": "7394edd67cb63be91848",
+    "url": "/React_Calculator/static/css/2.11da4523.chunk.css"
+  },
+  {
+    "revision": "7394edd67cb63be91848",
+    "url": "/React_Calculator/static/js/2.4af2c0cd.chunk.js"
+  },
+  {
+    "revision": "26562d995153cc5f19e58e505f2747d1",
+    "url": "/React_Calculator/static/media/pic1.26562d99.jpg"
+  },
+  {
+    "revision": "4a0e1133b4aae45d82da795577e016fd",
+    "url": "/React_Calculator/static/media/pic2.4a0e1133.png"
+  },
+  {
+    "revision": "057e424231fd4ec1c46c955e99d91939",
+    "url": "/React_Calculator/static/media/pic3.057e4242.jpg"
+  },
+  {
+    "revision": "e4acd2eec24ab4bb0d1b7517e85ea6a1",
+    "url": "/React_Calculator/static/media/cart.e4acd2ee.jpg"
+  },
+  {
+    "revision": "9f11fb577667afaea372cddc9fa6c326",
+    "url": "/React_Calculator/index.html"
+  }
+];
