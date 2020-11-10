ba64a868d3d8f8a7d35e5064f319069d6ae4d597
madhu <madhuvanthi88@gmail.com>
2020-11-10 15:47:25 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React_Calculator/precache-manifest.7c6ace439db3f140852793716c3099a2.js"
+  "/ShoppingCart_ReactJS/precache-manifest.b7d5b05f4b3a1ddc301183afc8f457b9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React_Calculator/index.html", {
+workbox.routing.registerNavigationRoute("/ShoppingCart_ReactJS/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
